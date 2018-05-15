d9b4166528ffecf725d28113e7494edf1d1eed1e
teezjay <teezjay11@gmail.com>
2018-05-16 02:59:33 +0300

    created def home() in jobs view.py created templates folder created a new path in urs setting in myportfolio created home.index in jobs/templates/jobs

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateField()
-    body = models.CharField(max_length=500)
+    pub_date = models.DateTimeField()
+    body = models.TextField(max_length=500)
     image = models.ImageField(upload_to='images/')
