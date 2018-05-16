54c8e8e25da3c4bad24af779ef3c8bdf0d5f0f9a
teezjay <teezjay11@gmail.com>
2018-05-16 06:55:25 +0300

    added more functionalities

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField(max_length=500)
     image = models.ImageField(upload_to='images/')
+    #a function to return a portion of text
+    def summary(self):
+        return self.body[:50]
+    #print time without the actual time
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    #prints titlein django admin as an understable manner
+    def __str__(self):
+        return self.title    
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import blog
 
 # Create your views here.
+def allblogs(request):
+    blogs = blog.objects
+    return render(request,'blogs/allblogs.html',{'blog':blogs})
